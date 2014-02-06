f8f8b43d467070197c6a1c73a255ea431f386dc0
koverholt <koverholt@0dc9d3fe-6b89-d991-a054-10efd4949ef8>
2014-02-06 20:15:23 +0000

    Smart FF: Add file logging to Arduino data logging script

--- Projects/Smart_Firefighting/Arduino/Data_Logger/receive_data.py
+++ Projects/Smart_Firefighting/Arduino/Data_Logger/receive_data.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python
 
 import pika
-import sys
 import time
+import argparse
+
+def callback(ch, method, properties, body):
+    print '%r' % (body)
+    with open('output.csv', 'a+') as text_file:
+        text_file.write(body+'\n')
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('broker', help='network or IP address of message broker')
+args = parser.parse_args()
 
 # Attemps to connect to server and run data receiving loop.
 # If it fails to connect to the broker, it will wait some time
 # and attempt to reconnect infinitely.
 while True:
     try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(
-                host='localhost'))
+        connection = pika.BlockingConnection(
+                    pika.ConnectionParameters(host=args.broker))
         channel = connection.channel()
-
         channel.exchange_declare(exchange='logs',
                                  type='fanout')
-
         result = channel.queue_declare(exclusive=True)
         queue_name = result.method.queue
-
         channel.queue_bind(exchange='logs',
                            queue=queue_name)
 
-        print ' [*] Waiting for logs. To exit press CTRL+C'
-
-        def callback(ch, method, properties, body):
-            print " [x] %r" % (body,)
+        print '[*] Waiting for logs. To exit press [CTRL] + [c] twice'
 
         channel.basic_consume(callback,
                               queue=queue_name,
@@ -33,5 +37,5 @@ while True:
 
         channel.start_consuming()
     except:
-        print "No broker found. Retrying in 5 seconds..."
+        print 'No broker found. Retrying in 5 seconds...'
         time.sleep(5)
--- Projects/Smart_Firefighting/Arduino/Data_Logger/send_data.py
+++ Projects/Smart_Firefighting/Arduino/Data_Logger/send_data.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python
 
 import pika
-import sys
 import time
+import argparse
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('broker', help='Network or IP address of message broker')
+parser.add_argument('logger_id', help='Logger name or ID')
+args = parser.parse_args()
 
 # Attemps to connect to server and run data broadcast loop.
 # If it fails to connect to the broker, it will wait some time
 # and attempt to reconnect infinitely.
 while True:
     try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(
-                host='192.168.1.100'))
+        connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=args.broker))
         channel = connection.channel()
 
         channel.exchange_declare(exchange='logs',
                                  type='fanout')
 
-        while 1:
-            message = ' '.join(sys.argv[1:]) or "ArduinoPumper: "+time.ctime()
+        while True:
+            message = (time.ctime()+',%s,200') % (args.logger_id)
             channel.basic_publish(exchange='logs',
                                   routing_key='',
                                   body=message)
+            print 'Sent %r' % (message)
+            with open('output.csv', 'a+') as text_file:
+                text_file.write(message+'\n')
             time.sleep(1)
-            print " [x] Sent %r" % (message,)
 
         connection.close()
     except:
-        print "No broker found. Retrying in 30 seconds..."
-        time.sleep(5)
+        print 'No broker found. Retrying in 30 seconds...'
+        time.sleep(30)
