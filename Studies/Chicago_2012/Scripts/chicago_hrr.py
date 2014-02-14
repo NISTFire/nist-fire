57c9cd6b85dca7692923e0b67204067eb63c8b43
craigweinschenk <craigweinschenk@gmail.com>
2014-02-14 21:33:19 +0000

    Chicago Fire: update to HRR plotting script

--- Studies/Chicago_2012/Scripts/chicago_hrr.py
+++ Studies/Chicago_2012/Scripts/chicago_hrr.py
@@ -10,31 +10,41 @@ def movingaverage(interval, window_size):
     window= np.ones(int(window_size))/float(window_size)
     return np.convolve(interval, window, 'same')
 
-
-C_FDS = np.genfromtxt('../FDS_Output_Files/west_50th_fire_hrr.csv', delimiter=',', skip_header=1, dtype=None, names=True)
+C_FDS = np.genfromtxt('../FDS_Output_Files/west_50th_baseline_hrr.csv', delimiter=',', skip_header=1, dtype=None, names=True)
 HRR = C_FDS['HRR']/1000
 HRR_avg = movingaverage(HRR,10)
+
+ND_FDS = np.genfromtxt('../FDS_Output_Files/west_50th_norear_hrr.csv', delimiter=',', skip_header=1, dtype=None, names=True)
+HRR_ND = ND_FDS['HRR']/1000
+HRR_ND_avg = movingaverage(HRR_ND,10)
 
 HRR_theo = [None]*1000
 HRR_time = list(xrange(1000))
 
 for i in range (0, 999):
-	if i <= 8:
-		HRR_theo[i] = (9/8)*i
-	elif i > 8 and i <= 40:
+	if i <= 10:
+		HRR_theo[i] = (9./10)*i
+	elif i > 10 and i <= 40:
 		HRR_theo[i] = 9
-	elif i > 40 and i <= 48:
-		HRR_theo[i] = 9 + (3.3/8)*(i-40)
+	elif i > 40 and i <= 50:
+		HRR_theo[i] = 9 + (3.3/10)*(i-40)
 	else:
 		HRR_theo[i] = 12.3
 
 print HRR_avg[129],HRR_avg[160]
 
 plt.figure
-plt.plot(C_FDS['Time'],HRR_avg,'k-',mfc='none',label='West 50th Fire Model HRR',linewidth=2)
-plt.plot(HRR_time,HRR_theo,'b',mfc='none',label='West 50th Fire Theory HRR',linewidth=2)
-plt.axvline(x=130, ymin=0, ymax=0.615,label='Back Door Open',linestyle=':',linewidth=3,color = '#CC5500')
-plt.axvline(x=160, ymin=0, ymax=0.615,label='Door Fail (Top)',linestyle='-.',linewidth=3,color = '#347235')
+plt.plot(HRR_time,HRR_theo,'b-.',mfc='none',label='Prescribed HRR',linewidth=2)
+plt.plot(C_FDS['Time'],HRR_avg,'r-',mfc='none',label='FDS Model HRR w/ Rear Door',linewidth=2)
+plt.plot(ND_FDS['Time'],HRR_ND_avg,'g--',mfc='none',label='FDS Model HRR w/o Rear Door',linewidth=2)
+plt.text(98, 2, 'Rear Door Open', 
+	 horizontalalignment='center',
+     verticalalignment='center')
+plt.text(195, 2, 'Interior Door Fail', 
+	 horizontalalignment='center',
+     verticalalignment='center')
+plt.axvline(x=130, ymin=0, ymax=12.3/20,linestyle='-',linewidth=2,color = '#000000')
+plt.axvline(x=160, ymin=0, ymax=12.3/20,linestyle='-',linewidth=2,color = '#000000')
 plt.axis([0, 250, 0, 20])
 plt.xlabel('Time (s)')
 plt.ylabel('HRR (MW)')
