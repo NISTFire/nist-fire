d95e2d61e745bdbf0062f9314438650f537807bc
koverholt <koverholt@0dc9d3fe-6b89-d991-a054-10efd4949ef8>
2014-03-14 21:45:55 +0000

    Arduino: Edits to send_data.py script

--- Projects/Smart_Firefighting/Arduino/send_data.py
+++ Projects/Smart_Firefighting/Arduino/send_data.py
@@ -8,12 +8,21 @@ import re
 
 
 def read_value(pin):
-    # Read voltage from analog pin
+    # Read voltage from specified analog pin over REST API
     response = urllib2.urlopen('http://localhost/arduino/analog/' + str(pin))
     value = response.read()
     value = re.findall('\d+', value)
     response.close()
     return float(value[1])
+
+def read_flow_rate(pin):
+    # Read voltage from specified analog pin
+    value = read_value(pin)
+    # Scale reading of 0-1023 to 0-5 V range
+    value = value / 1024 * 5
+    # Convert voltage to flow rate (gpm)
+    flow_rate = 17.669*value**3 + 414.39*value**2 + 3327.3*value - 9072
+    return voltage, flow_rate
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
@@ -35,14 +44,10 @@ while True:
                                  type='fanout')
 
         while True:
-            # Read voltage from pin 0
-            value = read_value(0)
-            # Scale reading of 0-1023 to 0-5 V range
-            value = value / 1024 * 5
-            # Convert voltage to flow rate (gpm)
-            flow_rate = 17.669*value**3 + 414.39*value**2 + 3327.3*value - 9072
+            # Read flow rate from pin 0
+            voltage, flow_rate = read_flow_rate(0)
             # Construct message for log
-            message = (time.ctime()+',%s,%0.2f,%0.2f') % (args.logger_id, value, flow_rate)
+            message = (time.ctime()+',%s,%0.2f,%0.2f') % (args.logger_id, voltage, flow_rate)
             channel.basic_publish(exchange='logs',
                                   routing_key='',
                                   body=message)
@@ -58,14 +63,10 @@ while True:
         timer = 0
         retry_value = 30
         while timer < retry_value:
-            # Read voltage from pin 0
-            value = read_value(0)
-            # Scale reading of 0-1023 to 0-5 V range
-            value = value / 1024 * 5
-            # Convert voltage to flow rate (gpm)
-            flow_rate = 17.669*value**3 + 414.39*value**2 + 3327.3*value - 9072
+            # Read flow rate from pin 0
+            voltage, flow_rate = read_flow_rate(0)
             # Construct message for log
-            message = (time.ctime()+',%s,%0.2f,%0.2f') % (args.logger_id, value, flow_rate)
+            message = (time.ctime()+',%s,%0.2f,%0.2f') % (args.logger_id, voltage, flow_rate)
             with open(args.log_file, 'a+') as text_file:
                 text_file.write(message+'\n')
             time.sleep(1)
