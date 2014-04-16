142afae08d51a225f11d6b350faac331669aff41
koverholt <koverholt@0dc9d3fe-6b89-d991-a054-10efd4949ef8>
2014-04-16 11:52:51 +0000

    DelCo: Edit Python plotting script

--- Projects/DelCo_2014_04/Scripts/plot.py
+++ Projects/DelCo_2014_04/Scripts/plot.py
@@ -20,6 +20,9 @@ data_file = '../Experimental_Data/test_data.csv'
 # Location of scaling conversion file
 scaling_file = '../DAQ_Files/Delco_DAQ_Channel_List.csv'
 
+# Duration of pre-test time (s)
+pre_test_time = 60
+
 # Common quantities for y-axis labelling
 heat_flux_quantities = ['HF_', 'RAD_']
 gas_quantities = ['CO_', 'CO2_', 'O2_']
@@ -37,6 +40,8 @@ scaling = pd.read_csv(scaling_file, index_col=2)
 
 # Generate a plot for each quantity
 for channel in data.dtype.names[2:]:
+    print 'Plotting ' + channel 
+
     scale_factor = float(scaling['Calibration'][channel])
 
     figure()
@@ -50,7 +55,8 @@ for channel in data.dtype.names[2:]:
         conv_inch_h2o = 0.4;
         conv_pascal = 248.8;
         # Convert voltage to pascals
-        pressure = conv_inch_h2o * conv_pascal * (data[channel])
+        pressure = conv_inch_h2o * conv_pascal * \
+                   (data[channel] - np.mean(data[channel])[0:pre_test_time])
         # Calculate velocity
         quantity = 0.0698 * np.sqrt(np.abs(pressure) * \
                    (data['TC_' + channel[4:]] + 273.15)) * np.sign(pressure)
