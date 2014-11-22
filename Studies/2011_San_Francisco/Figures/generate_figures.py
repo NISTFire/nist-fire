491f325ef9881d5ccad4da3bce53add4262663ad
koverholt <koverholt@0dc9d3fe-6b89-d991-a054-10efd4949ef8>
2014-11-22 01:35:33 +0000

    San Francisco: Minor edit to generate_figures script

--- Studies/2011_San_Francisco/Figures/generate_figures.py
+++ Studies/2011_San_Francisco/Figures/generate_figures.py
@@ -2,22 +2,16 @@
 
 from __future__ import division
 
-import numpy as np
 import pandas as pd
 from pylab import *
 
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
-
-def movingaverage(interval, window_size):
-    window= np.ones(int(window_size))/float(window_size)
-    return np.convolve(interval, window, 'same')
-
 FDS_file = '../FDS_Output_Files/133_berkeley_fire_hrr.csv'
 FDS = pd.read_csv(FDS_file, header=1)
 HRR_FDS = FDS['HRR'] / 1000
-HRR_FDS_avg = movingaverage(HRR_FDS, 10)
+HRR_FDS_avg = pd.rolling_mean(HRR_FDS, 10, center=True)
 
 CALC_file = '../Supplemental_Calculations/FDS_HRR_ramps.csv'
 CALC = pd.read_csv(CALC_file)
@@ -44,7 +38,7 @@ savefig('Fire_HRR.pdf')
 
 figure()
 plot(CALC['Time (s)'], HRR_CALC, 'k-', lw=2, label='Prescribed HRR')
-plot(FDS['Time'][:-4], HRR_FDS_avg[:-4], 'r--', lw=2, label='Calculated HRR')
+plot(FDS['Time'], HRR_FDS_avg, 'r--', lw=2, label='Calculated HRR')
 plt.text(342+10, 2, 'Rear Window Failures Begin')
 axvline(342, color='k', ls='--', lw=2)
 xlim([0, 540])
