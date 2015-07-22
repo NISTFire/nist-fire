95366d191573e6b4d2bc43b8aa119dbd47a55aa6
craigweinschenk <craigweinschenk@0dc9d3fe-6b89-d991-a054-10efd4949ef8>
2015-07-22 21:45:35 +0000

    Delco_2012: add rough gas cooling comp script

---
+++ Projects/DelCo_2012/Scripts/gas_cooling_plot2.py
@@ -0,0 +1,78 @@
+#Weinschenk
+#3-15
+
+import os
+import numpy as np
+import pandas as pd
+from pylab import *
+
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+
+# Location of experimental data files
+data_dir = '../Experimental_Data/'
+
+# Location of test description file
+info_file = '../Experimental_Data/Gas_Cooling_Test_Matrix.csv'
+
+# Location of figures
+plot_dir = '../Figures/Gas_Cooling/'
+# List of sensor groups for each plot
+sensor_groups = [['TC_A1_'],['TC_A3_']]
+
+# Load exp. timings and description file
+info = pd.read_csv(info_file, index_col=0)
+
+# Skip files
+skip_files = ['_reduced', 'description_','zero','bb','es_','fse','cafs','fsw','_attic','sc','gas_cooling','west_','east_','all_']
+
+#  ===============================
+#  = Loop through all data files =
+#  ===============================
+
+# Load exp. data file
+data = pd.read_csv(data_dir + 'GCSeries1.csv')
+test_name = 'GCSeries1'
+
+y = [1,2,3,4,5,6,7,8,9,10,11]
+
+del_TC_1_h2o = np.zeros((11,3))
+del_TC_3_h2o = np.zeros((11,3))
+del_TC_1_cafs = np.zeros((11,3))
+del_TC_3_cafs = np.zeros((11,3))
+
+
+for group in sensor_groups:
+	for channel in data.columns[1:]:
+		if 'TC_A1_' in channel:
+			del_TC_1_h2o[int(channel[6:])-1,0] = max(data[channel][1290:1350]) - min(data[channel][1290:1350])
+			del_TC_1_h2o[int(channel[6:])-1,1] = max(data[channel][1600:1700]) - min(data[channel][1600:1700])
+			del_TC_1_h2o[int(channel[6:])-1,2] = max(data[channel][2170:2250]) - min(data[channel][2170:2250])
+			del_TC_1_cafs[int(channel[6:])-1,0] = max(data[channel][2450:2550]) - min(data[channel][2450:2550])
+			del_TC_1_cafs[int(channel[6:])-1,1] = max(data[channel][2950:3000]) - min(data[channel][2950:3000])
+			del_TC_1_cafs[int(channel[6:])-1,2] = max(data[channel][3100:3150]) - min(data[channel][3100:3150])
+		if 'TC_A3_' in channel:
+			del_TC_3_h2o[int(channel[6:])-1,0] = max(data[channel][1290:1350]) - min(data[channel][1290:1350])
+			del_TC_3_h2o[int(channel[6:])-1,1] = max(data[channel][1600:1700]) - min(data[channel][1600:1700])
+			del_TC_3_h2o[int(channel[6:])-1,2] = max(data[channel][2170:2250]) - min(data[channel][2170:2250])
+			del_TC_3_cafs[int(channel[6:])-1,0] = max(data[channel][2450:2550]) - min(data[channel][2450:2550])
+			del_TC_3_cafs[int(channel[6:])-1,1] = max(data[channel][2950:3000]) - min(data[channel][2950:3000])
+			del_TC_3_cafs[int(channel[6:])-1,2] = max(data[channel][3100:3150]) - min(data[channel][3100:3150])
+del_water = concatenate([del_TC_1_h2o, del_TC_3_h2o], axis=1)
+del_cafs  = concatenate([del_TC_1_cafs, del_TC_3_cafs], axis=1)
+
+fig = figure()
+plot(del_water.mean(axis=1),y,'k',label='7/8 H$_2$O',linewidth=3)
+plt.fill_betweenx(y,del_water.mean(axis=1)+del_water.std(axis=1),del_water.mean(axis=1)-del_water.std(axis=1), facecolor='black',alpha=0.5,linewidth=3)
+plot(del_cafs.mean(axis=1),y,'r-',label='7/8 CAFS',linewidth=3)
+plt.fill_betweenx(y,del_cafs.mean(axis=1)+del_cafs.std(axis=1),del_cafs.mean(axis=1)-del_cafs.std(axis=1), facecolor='red',alpha=0.25,linewidth=3)
+ax1 = gca()
+xlabel('$\Delta$T ($^{\circ}$C)')
+ylabel('Height')
+xticks(fontsize=16)
+yticks(fontsize=16)
+legend(numpoints=1,loc=1,fontsize=16)
+axis([0, 85, 0, 12])
+grid(True)
+savefig(plot_dir + test_name + '_delta_T.pdf',format='pdf')
+
