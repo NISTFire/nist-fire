8bf7fcebac53397f9c2fb12f8888c76593028329
cweinschenk <craigweinschenk@gmail.com>
2015-09-10 19:01:01 -0400

    messing around with ggplot

--- Projects/DelCo_2012/Scripts/gas_cooling_plot2.py
+++ Projects/DelCo_2012/Scripts/gas_cooling_plot2.py
@@ -6,8 +6,12 @@ import numpy as np
 import pandas as pd
 from pylab import *
 import statsmodels.api as sm
+
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
+
+from ggplot import *
+from patsy import ModelDesc
 
 # Location of experimental data files
 data_dir = '../Experimental_Data/'
@@ -72,10 +76,13 @@ for entry in range(0,len(info)):
 						cafs_data_interval = cafs_test[channel][info['Start_Time_CAFS'][entry+reps]:info['Stop_Time_CAFS'][entry+reps]]
 						del_TC_3_cafs[int(channel[6:])-1,reps] = max(cafs_data_interval) - min(cafs_data_interval)
 		del_water = concatenate([del_TC_1_h2o, del_TC_3_h2o], axis=1)
+		del_water = del_water.reshape(-1)
 		del_cafs  = concatenate([del_TC_1_cafs, del_TC_3_cafs], axis=1)
-
-
-
+		del_cafs  = del_cafs.reshape(-1)
+		dif_data  = []
+		for i in range (0,len(del_water)):
+			dif_data.append({'del_water' : del_water[i], 'del_cafs' : del_cafs[i]})
+		del_temp = pd.DataFrame(dif_data,columns=['del_water','del_cafs'])
 
 		#-----------------------
 		# Stats Analysis OLS
@@ -90,15 +97,19 @@ for entry in range(0,len(info)):
 		y_hat = est.predict(X_prime)
 		avg_rel_diff = sum((y-X)/((y+X)/2.))/len(X)
 
-		fig = figure()
-		plt.scatter(X,y,alpha=0.5)
-		plt.plot(X_prime, y_hat, 'r', alpha=0.9)  # Add the regression line, colored in red
-		plt.plot(X_prime,X_prime,'k')
-		axis([0, max_axis, 0, max_axis])
-		plt.text(0.1*max_axis , 0.95*max_axis, 'R$^2$ = ' + str(around(est.rsquared,decimals=3)),
-			 horizontalalignment='center',
-		     verticalalignment='center', bbox=dict(facecolor='none', edgecolor='black'))
-		xlabel('$\Delta$T ($^{\circ}$C) CAFS')
-		ylabel('$\Delta$T ($^{\circ}$C) H$_2$O')
-		savefig(plot_dir + info['Nozzle'][entry] + '_' + info['Position'][entry] + '_scatter.pdf',format='pdf')
+		p = ggplot(aes('del_cafs', 'del_water'),data = del_temp) + geom_point() + stat_smooth(method='lm', color='blue',se=True) + \
+		scale_x_continuous("$\Delta$T (deg C) CAFS", limits=(0,max_axis)) + scale_y_continuous("$\Delta$T (deg C) H$_2$O",limits=(0,max_axis))
+		ggsave(p,plot_dir + info['Nozzle'][entry] + '_' + info['Position'][entry] + '_scatter_ggplot.pdf',format='pdf' )
+
+		# fig = figure()
+		# plt.scatter(X,y,alpha=0.5)
+		# plt.plot(X_prime, y_hat, 'r', alpha=0.9)  # Add the regression line, colored in red
+		# plt.plot(X_prime,X_prime,'k')
+		# axis([0, max_axis, 0, max_axis])
+		# plt.text(0.1*max_axis , 0.95*max_axis, 'R$^2$ = ' + str(around(est.rsquared,decimals=3)),
+		# 	 horizontalalignment='center',
+		#      verticalalignment='center', bbox=dict(facecolor='none', edgecolor='black'))
+		# xlabel('$\Delta$T ($^{\circ}$C) CAFS')
+		# ylabel('$\Delta$T ($^{\circ}$C) H$_2$O')
+		#savefig(plot_dir + info['Nozzle'][entry] + '_' + info['Position'][entry] + '_scatter.pdf',format='pdf')
 
