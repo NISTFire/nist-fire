7d7d6fc47c04a92cd3084e14dce08fefc4ba4984
Willi <willijm92>
2016-03-07 09:32:08 -0500

    Updated FF sensor helmet plotting script with new Bokeh commands

--- Equipment/FF_Sensor_Helmet/Scripts/plot_helmet_data.py
+++ Equipment/FF_Sensor_Helmet/Scripts/plot_helmet_data.py
@@ -2,8 +2,11 @@
 import time
 import numpy as np
 import pandas as pd
-from bokeh.plotting import figure,vplot,show,output_file,ColumnDataSource,output_server,cursession
+from bokeh.plotting import *
+from bokeh.plotting import figure,vplot,show,output_file,ColumnDataSource,output_server,curdoc
 from bokeh.models import HoverTool, LinearAxis, Range1d, GlyphRenderer
+from bokeh.client import push_session
+# from bokeh.embed import autoload_server
 
 # Bokeh options
 TOOLS="pan,box_zoom,wheel_zoom,reset,resize,save,hover"
@@ -15,32 +18,34 @@ T_data = np.array([])
 HF_data = np.array([])
 
 p1 = figure(title='FF Helmet - Temperature', x_axis_label = 'Time (s)', y_axis_label = 'Temperature (°C)',
-	tools=TOOLS, plot_width=1900, plot_height=1300)
-p1.line(time_x, T_data, color="#dd0022", line_width = 5)
+	tools=TOOLS, plot_width=900, plot_height=500)
+p1.line(time_x, T_data, color="#dd0022", line_width = 4)
 # legend='Amb T')
-p1.xaxis.axis_label_text_font_size = '50pt'
-p1.xaxis.major_label_text_font_size = '40pt'
-p1.yaxis.major_label_text_font_size = '40pt'
-p1.yaxis.axis_label_text_font_size = '50pt'
-p1.yaxis.axis_label_standoff = 25
-p1.title_text_font_size = '55pt'
+p1.xaxis.axis_label_text_font_size = '20pt'
+p1.xaxis.major_label_text_font_size = '15pt'
+p1.yaxis.major_label_text_font_size = '15pt'
+p1.yaxis.axis_label_text_font_size = '20pt'
+p1.yaxis.axis_label_standoff = 10
+p1.title_text_font_size = '24pt'
 # p1.title_label_standoff = 20
 
 p2 = figure(title='FF Helmet - Heat Flux', x_axis_label = 'Time (s)', y_axis_label = 'Heat Flux (kW/m²)',
-	tools=TOOLS, plot_width=1900, plot_height=1300)
-p2.line(time_x, HF_data, color="#0000dd", line_width = 7, line_dash = 'dashed') 
+	tools=TOOLS, plot_width=900, plot_height=500)
+p2.line(time_x, HF_data, color="#0000dd", line_width = 4, line_dash = 'dashed') 
 #legend='Heat Flux')
 
-p2.xaxis.axis_label_text_font_size = '50pt'
-p2.xaxis.major_label_text_font_size = '40pt'
-p2.yaxis.major_label_text_font_size = '40pt'
-p2.yaxis.axis_label_text_font_size = '50pt'
-p2.yaxis.axis_label_standoff = 25
-p2.title_text_font_size = '55pt'
+p2.xaxis.axis_label_text_font_size = '20pt'
+p2.xaxis.major_label_text_font_size = '15pt'
+p2.yaxis.major_label_text_font_size = '15pt'
+p2.yaxis.axis_label_text_font_size = '20pt'
+p2.yaxis.axis_label_standoff = 10
+p2.title_text_font_size = '24pt'
 
 p = vplot(p1, p2)
 
-show(p)
+# show(p)
+session = push_session(curdoc())
+# autoload_server(p, session_id=session.id)
 
 renderer = p1.select(dict(type=GlyphRenderer))
 ds1 = renderer[0].data_source
@@ -48,21 +53,31 @@ ds1 = renderer[0].data_source
 renderer = p2.select(dict(type=GlyphRenderer))
 ds2 = renderer[0].data_source
 
-while True:
-	# Update with latest HF and T readings
-	ds1.data["x"] = time_x
-	ds1.data["y"] = T_data
-	ds1._dirty = True
-	cursession().store_objects(ds1)
-
-	ds2.data["x"] = time_x
-	ds2.data["y"] = HF_data
-	ds2._dirty = True
-	cursession().store_objects(ds2)
-
-	time.sleep(1)
-
+def update():
 	new_data = pd.read_csv('../Data/output.csv', index_col=0)
 	time_x = new_data.iloc[ : , 1]
 	T_data = new_data.iloc[ : , 2]
 	HF_data = new_data.iloc[ : , 3]
+	# Update with latest HF and T readings
+	ds1.data["x"] = time_x
+	ds1.data["y"] = T_data
+	ds1._dirty = True
+	# cursession().store_objects(ds1)
+	# push_session(ds1)
+
+	ds2.data["x"] = time_x
+	ds2.data["y"] = HF_data
+	ds2._dirty = True
+	# push_session(ds2)
+	# cursession().store_objects(ds2)
+
+	# time.sleep(1)
+
+curdoc().add_periodic_callback(update, 1000)
+
+session.show()
+
+session.loop_until_closed()
+
+
+
