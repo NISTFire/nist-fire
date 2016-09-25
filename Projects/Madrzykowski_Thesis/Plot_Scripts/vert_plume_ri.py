dda6815a7a750eca6cc7d8fcaff4f7a58ecee0af
cweinschenk <craigweinschenk@gmail.com>
2016-09-25 13:29:44 -0400

    add error bars to ri plots

--- Projects/Madrzykowski_Thesis/Plot_Scripts/vert_plume_ri.py
+++ Projects/Madrzykowski_Thesis/Plot_Scripts/vert_plume_ri.py
@@ -13,20 +13,20 @@ y_data = [0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4]
 y_fds = [0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2]
 
 test_name = 'IWGB_NG_TC_Plume_Avg'
-fds_name = 'plumetemp_avg_2D'
+fds_name = 'plumetemp_avg_0D'
 distance = ['_0D','_0pD','_1D','_2D']
 
 data = pd.read_csv(data_dir+test_name+'.csv', header=0)
 fds  = pd.read_csv(fds_dir+fds_name+'.csv', header=0)
 
 
-data_slice = np.squeeze(np.asarray(data.iloc[[3],1:]))
+data_slice = np.squeeze(np.asarray(data.iloc[[0],1:]))
 fds_slice_05 = np.squeeze(np.asarray(fds.iloc[[0],1:]))
 fds_slice_10 = np.squeeze(np.asarray(fds.iloc[[1],1:]))
 fds_slice_20 = np.squeeze(np.asarray(fds.iloc[[2],1:]))
 
 fig = figure()
-plot(data_slice,y_data,linestyle='None',marker='o',ms=8,color='k',label='Experiment')
+errorbar(data_slice,y_data,xerr=0.15*data_slice,linestyle='None',marker='o',ms=8,color='k',label='Experiment')
 plot(fds_slice_05,y_fds,'k',label = 'FDS 05')
 plot(fds_slice_10,y_fds,'r',label = 'FDS 10')
 plot(fds_slice_20,y_fds,'b',label = 'FDS 20')	
@@ -39,6 +39,6 @@ grid(True)
 ax = gca()
 axis([0, 850, 0, 2.2])
 legend(numpoints=1,loc='upper right',fontsize=16 )
-savefig(plot_dir + test_name + distance[3] + '_RI.pdf',format='pdf')
+savefig(plot_dir + test_name + distance[0] + '_RI.pdf',format='pdf')
 close()
 
