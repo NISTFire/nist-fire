ef7628c708fa6b7537c90f110fad079f11f9df26
cweinschenk <craigweinschenk@gmail.com>
2017-03-12 12:03:52 -0400

    update plot script

--- Projects/Madrzykowski_Thesis/Plot_Scripts/vert_center_hf_ri.py
+++ Projects/Madrzykowski_Thesis/Plot_Scripts/vert_center_hf_ri.py
@@ -6,7 +6,7 @@ from pylab import *
 from itertools import cycle
 rcParams.update({'figure.autolayout': True})
 
-data_dir = '../Experimental_Data/Vertical_Comp/'
+data_dir = '../Experimental_Data/IWGBNG/'
 fds_dir = '../FDS_Output_Files/'
 plot_dir = '../Figures/'
 
@@ -14,19 +14,21 @@ y_data = [0.2,0.4,0.6,0.8,1.0,1.2]
 y_fds = [0.2,0.4,0.6,0.8,1.0,1.2]
 
 test_name = 'IWGB_NG_HF_Center_Avg'
-fds_name = 'hfwallcl_avg_2D'
+fds_name = 'hfwallcl_avg'
 distance = ['_0D','_0p5D','_1D','_2D']
 
-data = pd.read_csv(data_dir+test_name+'.csv', header=0)
-fds  = pd.read_csv(data_dir+test_name+'_FDS.csv', header=0)
+for i in range(len(distance)):
+	data = pd.read_csv(data_dir+test_name+'.csv', header=0)
+	fds  = pd.read_csv(fds_dir+fds_name+distance[i]+'.csv', header=0)
 
-for i in range(4):
 	data_slice = np.squeeze(np.asarray(data.iloc[[i],1:]))
-	fds_slice = np.squeeze(np.asarray(fds.iloc[[i],1:]))
+	fds_slice_05 = np.squeeze(np.asarray(fds.iloc[[0],1:]))
+	fds_slice_10 = np.squeeze(np.asarray(fds.iloc[[1],1:]))
 
 	fig = figure()
 	errorbar(data_slice,y_data,xerr=0.15*data_slice,linestyle='None',marker='o',ms=8,color='k',label='Experiment')
-	plot(fds_slice,y_fds,'k',label = 'FDS')
+	plot(fds_slice_05,y_fds,'k',label = 'FDS 05')
+	plot(fds_slice_10,y_fds,'r',label = 'FDS 10')
 	ax1 = gca()
 	xlabel('Heat Flux (kW/m$^{2}$)', fontsize=20)
 	ylabel('Distance above Burner (m)', fontsize=20)
@@ -34,7 +36,7 @@ for i in range(4):
 	yticks(fontsize=16)
 	grid(True)
 	ax = gca()
-	axis([0, 45, 0, 1.2])
+	axis([0, 60, 0, 1.4])
 	legend(numpoints=1,loc='upper right',fontsize=16 )
 	savefig(plot_dir + test_name + distance[i] + '_RI.pdf',format='pdf')
 	close()
